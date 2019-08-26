9083c77a40403617e971d3f8bcac206ad2502fbd
bookgvi <bookgvi@gmail.com>
2019-08-27 02:56:24 +0300

    The GAme of Life v1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-stage :config="stage">
-      <v-layer>
+      <v-layer ref="edem">
         <life-elem-col
           v-for="i in lifeCols"
           :key="i"
@@ -16,8 +16,30 @@
 import { mapGetters, mapActions } from 'vuex'
 import lifeElemCol from './components/lifeElemCol'
 export default {
+  data: () => ({
+    isEvolution: false,
+    lifeTimer: 0
+  }),
   created () {
     this.concieve()
+  },
+  mounted () {
+    this.$refs.edem.getNode().on('click', (e) => {
+      let col = Math.floor((e.target.index) / this.lifeRows)
+      let row = (e.target.index) % this.lifeRows
+      if (this.lifeElem[col][row].isAlive) {
+        this.die({ col, row })
+      } else {
+        this.born({ col, row })
+      }
+      this.isEvolution.start()
+    })
+    this.isEvolution = new Konva.Animation(frame => {
+      if (frame.time - this.lifeTimer > 3000) {
+        this.lifeTimer = Math.max(this.lifeTimer, frame.time)
+        this.lifeCicle()
+      }
+    })
   },
   components: {
     'life-elem-col': lifeElemCol
@@ -25,13 +47,26 @@ export default {
   computed: {
     ...mapGetters([
       'stage',
-      'lifeCols'
+      'lifeElem',
+      'lifeCols',
+      'lifeRows'
     ])
   },
   methods: {
     ...mapActions([
-      'concieve'
-    ])
+      'concieve',
+      'born',
+      'die'
+    ]),
+    lifeCicle () {
+      for (let c = 0; c < this.lifeCols; c++) {
+        for (let r = 0; r < this.lifeRows; r++) {
+          if (this.lifeElem[c][r].isAlive) {
+            this.lifeElem[c][r + 1] ? this.born({ col: c, row: r + 1 }) : ' '
+          }
+        }
+      }
+    }
   }
 }
 </script>
